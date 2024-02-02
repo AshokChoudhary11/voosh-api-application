f16fca853828cb1a18076ee1ec8249ff73f3e68e
Ashok <ashokchoudharya86@gmail.com>
2024-02-02 12:39:56 +0530

    add validation and add forign key

--- model.js
+++ model.js
@@ -4,15 +4,42 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const OrderSchema = new Schema({
-  sub_total: Number,
-  phone_number: Number,
-  user_id: String,
+  sub_total: {
+    type: Number,
+    required: true,
+  },
+  phone_number: {
+    type: Number,
+    unique: true,
+    required: true,
+    min: [1000000000, "Phone number must be at least 10 digits"], // Minimum length
+    max: 9999999999, // Maximum length
+    validate: {
+      validator: function (v) {
+        return /^\d{10}$/.test(v.toString()); // Custom validator for exact length of 10 digits
+      },
+      message: "Phone number must be exactly 10 digits",
+    },
+  },
+  user_id: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User", // Reference
+  },
 });
 
 const UserSchema = new Schema({
   phone_number: {
     type: Number,
     unique: true,
+    required: true,
+    min: [1000000000, "Phone number must be at least 10 digits"], // Minimum length
+    max: 9999999999, // Maximum length
+    validate: {
+      validator: function (v) {
+        return /^\d{10}$/.test(v.toString()); // Custom validator for exact length of 10 digits
+      },
+      message: "Phone number must be exactly 10 digits",
+    },
   },
   password: {
     type: String,
